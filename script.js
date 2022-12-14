99be88c0df46c1ff4b6bb313c2698eaa81dc9dba
Nash Sibanda <nash.sibanda@makeleaps.com>
2022-12-14 16:57:13 +0000

    formatting

--- script.js
+++ script.js
@@ -1,59 +1,69 @@
 let url = "";
 let prefix;
-const prefixEntry = document.getElementById("prefix")
+const prefixEntry = document.getElementById("prefix");
 
 async function init() {
-  const tabs = await chrome.tabs.query({active: true, lastFocusedWindow: true})
+  const tabs = await chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+  });
   url = tabs[0].url;
   addBranchName();
 }
 
-const branchNameDisplay = document.getElementById("branch-name")
-const branchCommandDisplay = document.getElementById("branch-command")
-const container = document.getElementById("container")
+const branchNameDisplay = document.getElementById("branch-name");
+const branchCommandDisplay = document.getElementById("branch-command");
+const container = document.getElementById("container");
 
 function addBranchName() {
   if (url.includes("trello.com/c/")) {
-    container.classList.remove("hide")
-    let trimmed = url.replace(/\/c\/([\d\w]*)\//, "tr-$1/").replace("http://trello.com", "").replace("https://trello.com", "").replace(/\/\d+-/i, "/")
-    let withPrefix = `${prefix ? prefix : "ml"}/${trimmed}`.replace(/^(.{60}[^-]*).*/, "$1")
+    container.classList.remove("hide");
+    let trimmed = url
+      .replace(/\/c\/([\d\w]*)\//, "tr-$1/")
+      .replace("http://trello.com", "")
+      .replace("https://trello.com", "")
+      .replace(/\/\d+-/i, "/");
+    let withPrefix = `${prefix ? prefix : "ml"}/${trimmed}`.replace(
+      /^(.{60}[^-]*).*/,
+      "$1"
+    );
     branchNameDisplay.innerText = withPrefix;
     branchCommandDisplay.innerText = "git checkout -b " + withPrefix;
   } else {
-    container.classList.add("hide")
+    container.classList.add("hide");
   }
 }
 
-getData()
+getData();
 
 function getData() {
-    chrome.storage.sync.get('prefix', function ( data ) {
-        prefix = data.prefix;
-        prefixEntry.value = prefix || "";
-    })
+  chrome.storage.sync.get("prefix", function (data) {
+    prefix = data.prefix;
+    prefixEntry.value = prefix || "";
+  });
 }
 
 function savePrefix() {
   prefix = prefixEntry.value.replace(/[^\da-zA-Z]/g, "");
-  
-  chrome.storage.sync.set( {
+
+  chrome.storage.sync.set({
     prefix: prefix,
-  } )
+  });
   addBranchName();
 }
 
 const doneBtn = document.getElementById("done-btn");
-doneBtn.addEventListener('click', () => {
-  savePrefix()
+doneBtn.addEventListener("click", () => {
+  savePrefix();
 });
 
-prefixEntry.addEventListener('input', () => {
-  savePrefix()
+prefixEntry.addEventListener("input", () => {
+  savePrefix();
 });
 
 const copyBtn = document.getElementById("copy-btn");
-copyBtn.addEventListener('click', () => {
-  copyToClipboard(branchNameDisplay.innerText)
+copyBtn.addEventListener("click", () => {
+  copyToClipboard(branchNameDisplay.innerText);
 });
 
 function copyToClipboard(text) {
@@ -61,12 +71,12 @@ function copyToClipboard(text) {
 }
 
 const copyCmdBtn = document.getElementById("copy-cmd-btn");
-copyCmdBtn.addEventListener('click', () => {
-  copyToClipboard()
+copyCmdBtn.addEventListener("click", () => {
+  copyToClipboard();
 });
 
 function copyToClipboard() {
   navigator.clipboard.writeText(branchCommandDisplay.innerText);
 }
 
-init()
+init();
